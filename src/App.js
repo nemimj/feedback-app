eaa0e20c4753e45b0152ef40a0295b1f03ea8922
nemi <nemimj123@gmail.com>
2023-10-26 12:42:43 +0530

    Resetting app.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
 function App() {
   // empty tag is called fragment tag
 
-  const title = "First blog";
-  const body = "this is my first blog iam so excited";
-  const showComments = true;
-  const comments = [
-    { id: 1, text: "HEy good" },
-    { id: 2, text: "You are too good" },
-    { id: 3, text: "where clever" },
-  ];
-  const commentBlock = (
-    <div className="comments">
-      <h3>Comments {comments.length}</h3>
-      <ul>
-        {comments.map((comment) => (
-          <li key={comment.id}>{comment.text} </li>
-        ))}
-      </ul>
-    </div>
-  );
-
   return (
     <div className="container">
-      <h1>{title}</h1>
-      <p>{body}</p>
-      {showComments && commentBlock}
+      <h1>My App</h1>
     </div>
   );
 }
