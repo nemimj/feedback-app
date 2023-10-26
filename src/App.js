ba11c043faa50c724ebeeb1e413fbc898ac1d975
nemi <nemimj123@gmail.com>
2023-10-26 12:41:32 +0530

    added conditonals and dyanmic variables

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 function App() {
   // empty tag is called fragment tag
+
+  const title = "First blog";
+  const body = "this is my first blog iam so excited";
+  const showComments = true;
+  const comments = [
+    { id: 1, text: "HEy good" },
+    { id: 2, text: "You are too good" },
+    { id: 3, text: "where clever" },
+  ];
+  const commentBlock = (
+    <div className="comments">
+      <h3>Comments {comments.length}</h3>
+      <ul>
+        {comments.map((comment) => (
+          <li key={comment.id}>{comment.text} </li>
+        ))}
+      </ul>
+    </div>
+  );
+
   return (
     <div className="container">
-      <h1>Hello world </h1>
-      <p>This is my first react app</p>
+      <h1>{title}</h1>
+      <p>{body}</p>
+      {showComments && commentBlock}
     </div>
   );
 }
