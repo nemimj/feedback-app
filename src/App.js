3bbfb5821224ec398e4a1536afab147d7cc11054
nemi <nemimj123@gmail.com>
2023-10-27 13:25:37 +0530

    added props

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <Header></Header>
+      <Header text="hello world!"></Header>
       <div className="container">
         <h1>My App</h1>
       </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,20 @@
 import React from "react";
-
-function Header() {
+import PropTypes from "prop-types";
+function Header(props) {
   return (
     <header>
-      <h2>feedback UI</h2>
+      <div className="container">
+        <h2>{props.text}</h2>
+      </div>
     </header>
   );
 }
 
+Header.defaultProps = {
+  text: "Feedback UI",
+};
+
+Header.prototype = {
+  text: PropTypes.string,
+};
 export default Header;
