f9087f7762dc4e7eed0ae8a73bf5a21f4cd2a92e
nemi <nemimj123@gmail.com>
2023-10-27 11:11:59 +0530

    added components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import Header from "./components/Header.jsx";
+
 function App() {
   // empty tag is called fragment tag
 
   return (
-    <div className="container">
-      <h1>My App</h1>
-    </div>
+    <>
+      <Header></Header>
+      <div className="container">
+        <h1>My App</h1>
+      </div>
+    </>
   );
 }
 
