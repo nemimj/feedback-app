a02254ca127c31b0dc03000f102f8e5bc254b408
nemi <nemimj123@gmail.com>
2023-10-27 13:58:34 +0530

    added state and usestate

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import { useState } from "react";
 import Header from "./components/Header.jsx";
-
+import FeedbackItem from "./components/FeedbackItem.jsx";
+import FeedbackData from "./data/feedbackData.jsx";
 function App() {
   // empty tag is called fragment tag
-
+  const [feedback, setFeedback] = useState(FeedbackData);
   return (
     <>
       <Header></Header>
       <div className="container">
         <h1>My App</h1>
+        <FeedbackItem></FeedbackItem>
       </div>
     </>
   );
