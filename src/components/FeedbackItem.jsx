4ae8e4d95b0cdab72c7f86ea953684a10a2835cc
nemi <nemimj123@gmail.com>
2023-10-28 10:58:15 +0530

    added button onclick event in feedbackitem.jsx

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import Card from "./shared/Card";
 import { FaTimes } from "react-icons/fa";
 function FeedbackItem({ item }) {
+  const handleClick = (id) => {
+    console.log(id);
+  };
+
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
-      <button className="close">
+      <button className="close" onClick={() => handleClick(item.id)}>
         <FaTimes color="purple"></FaTimes>
       </button>
       <div className="text-display">{item.text}</div>
