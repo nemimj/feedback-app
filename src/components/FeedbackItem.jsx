73e5c5e1002103abd1ce618ae093e0e61d21bbb8
nemi <nemimj123@gmail.com>
2023-10-28 10:48:44 +0530

    added icons to feedbackitem.jsx

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import Card from "./shared/Card";
+import { FaTimes } from "react-icons/fa";
 function FeedbackItem({ item }) {
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
+      <button className="close">
+        <FaTimes color="purple"></FaTimes>
+      </button>
       <div className="text-display">{item.text}</div>
     </Card>
   );
