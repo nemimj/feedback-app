8ee6382bd2a46204cfec5a38952c3242756168d2
nemi <nemimj123@gmail.com>
2023-10-28 11:09:28 +0530

    added handleDelete in feedbackList.jsx

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import Card from "./shared/Card";
 import { FaTimes } from "react-icons/fa";
-function FeedbackItem({ item }) {
-  const handleClick = (id) => {
-    console.log(id);
-  };
-
+function FeedbackItem({ item, handleDelete }) {
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
-      <button className="close" onClick={() => handleClick(item.id)}>
+      <button className="close" onClick={() => handleDelete(item.id)}>
         <FaTimes color="purple"></FaTimes>
       </button>
       <div className="text-display">{item.text}</div>
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import FeedbackItem from "./FeedbackItem";
 function FeedbackList({ feedback }) {
+  // feedback = feedback.filter((item)=>item.id !== state)
+
   if (!feedback || feedback.lenght === 0) {
     return <p>No Feedback Yet</p>;
   }
   return (
     <div className="feedback-list">
       {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item}></FeedbackItem>
+        <FeedbackItem
+          key={item.id}
+          item={item}
+          handleDelete={(id) => console.log(id)}
+        ></FeedbackItem>
       ))}
     </div>
   );
