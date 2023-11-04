e5230012b206ac55aff3c3cde2d354a7a29f69db
nemi <nemimj123@gmail.com>
2023-11-04 12:19:33 +0530

    added the useEffect in ratingSelect.jsx

--- src/components/RatingSelect.jsx
+++ src/components/RatingSelect.jsx
@@ -1,12 +1,22 @@
 import React from "react";
-import { useState } from "react";
+import { useState,useContext,useEffect } from "react";
+import FeedbackContext from "../context/FeedbackContext";
 function RatingSelect({ select }) {
   const [selected, setSelected] = useState(10);
+
+  const {feedbackEdit} = useContext(FeedbackContext); 
+
+  useEffect(()=>{
+   setSelected(feedbackEdit.item.rating) 
+  },[feedbackEdit])
 
   const handleChange = (e) => {
     setSelected(+e.currentTarget.value); // ! by default the value will be in string so we use +
     select(+e.currentTarget.value);
   };
+
+
+   
 
   return (
     <ul className="rating">
